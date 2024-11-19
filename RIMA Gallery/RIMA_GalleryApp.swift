ea670171798be097a198ee4b21220e4eccf4bc28
Rafael Magaña <raismaav.2510@gmail.com>
2024-11-19 15:43:31 -0600

    Initial Commit

---
+++ RIMA Gallery/RIMA_GalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIMA_GalleryApp.swift
+//  RIMA Gallery
+//
+//  Created by Rafael Magana  on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct RIMA_GalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
